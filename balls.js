1f41fcbd83d39936db882a74ad9d8faa76c6cfb8
iTop <itop@192-168-0-100.rdsnet.ro>
2019-01-15 22:57:11 +0200

    refactor Ball object to use ES6 syntax

--- balls.js
+++ balls.js
@@ -32,55 +32,63 @@ class Ball extends Shape {
         this.color = color;
         this.size = size;
     }
+
+    // define ball draw method
+    draw () {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    };
+    
+    // define ball update method
+    update () {
+        if((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+        }
+    
+        if((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+        }
+    
+        if((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+        }
+    
+        if((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+        }
+    
+        this.x += this.velX;
+        this.y += this.velY;
+    };
+
+    // define ball collision detection
+    collisionDetect () {
+        for(var j = 0; j < balls.length; j++) {
+        if(!(this === balls[j])) {
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+    
+            if (distance < this.size + balls[j].size) {
+            balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+            }
+        }
+        }
+    };
+
 }
-
-// define ball draw method
-
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-// define ball update method
-
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
-
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-};
-
-// define ball collision detection
-
-Ball.prototype.collisionDetect = function() {
-  for(var j = 0; j < balls.length; j++) {
-    if(!(this === balls[j])) {
-      var dx = this.x - balls[j].x;
-      var dy = this.y - balls[j].y;
-      var distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
-      }
+// define EvilCircle constructor 
+class EvilCircle extends Shape {
+    constructor(x, y, velX, velY,exists){
+        super(x, y, velX, velY,exists);
+        this.velX = 20;
+        this.velY = 20;
+        this.color = 'white';
+        this.size = 10;
     }
-  }
-};
+}
 
 // define array to store balls
 
